3f7f3da84b873e7bc4888df500003e42bab6b003
Basir Ahmad Omid <102549962+BasirOmid@users.noreply.github.com>
2022-07-03 23:13:03 +0300

    Add files via upload

---
+++ Ziraat_Mobil.dpr
@@ -0,0 +1,14 @@
+program Ziraat_Mobil;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Banka in 'Banka.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
